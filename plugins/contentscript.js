6fb02483fc8d9b57efc64a0d446f311d51299121
Kazunori Kajihiro <likerichie@gmail.com>
2012-04-10 20:26:00 +0900

    remvoed unnecessary files

--- plugins/contentscript.js
+++
@@ -1,5 +0,0 @@
-vichrome.NormalMode.prototype.reqPluginSample = function() {
-    alert('hello, sample plugin command called!');
-};
-
-vichrome.CommandExecuter.prototype.commandTable.PluginSample = "triggerInsideContent";
